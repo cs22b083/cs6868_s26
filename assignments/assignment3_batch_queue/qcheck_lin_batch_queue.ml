d23b720c2395585b13ffe0d4515dbaef463c6b76
cs22b083 <cs22b083@smail.iitm.ac.in>
2026-04-02 10:06:51 +0530

    Qcheck_lin test

--- assignments/assignment3_batch_queue/qcheck_lin_batch_queue.ml
+++ assignments/assignment3_batch_queue/qcheck_lin_batch_queue.ml
@@ -27,15 +27,30 @@ module BQ = BatchQueue
 (** Lin API specification for the batch queue *)
 module BatchQueueSig = struct
   type t = int BQ.t
-
+   (* Create a queue with capacity 6 for testing *)
   let init () = BQ.create 6
 
   let cleanup _ = ()
 
+  let try_enq1 q x = BQ.try_enq q [| x |]
+  let try_enq2 q x y = BQ.try_enq q [| x; y |]
+  let try_enq3 q x y z = BQ.try_enq q [| x; y; z |]
+
+  let try_deq1 q = BQ.try_deq q 1
+  let try_deq2 q = BQ.try_deq q 2
+  let try_deq3 q = BQ.try_deq q 3
+
   open Lin
   let int_small = nat_small
   let _ = (t, int_small)
-  let api = failwith "TODO: define wrapper functions and api list (see README Part 3)"
+  let api = [
+    val_ "try_enq1" try_enq1 (t @-> int_small @-> returning bool);
+    val_ "try_enq2" try_enq2 (t @-> int_small @-> int_small @-> returning bool);
+    val_ "try_enq3" try_enq3 (t @-> int_small @-> int_small @-> int_small @-> returning bool);
+    val_ "try_deq1" try_deq1 (t @-> returning (option (array int_small)));
+    val_ "try_deq2" try_deq2 (t @-> returning (option (array int_small)));
+    val_ "try_deq3" try_deq3 (t @-> returning (option (array int_small)));
+  ]
 end
 
 (** Generate the linearizability test from the specification *)
