287685e0169f636e67a5b05cb18f439f6e5f3973
cs22b083 <cs22b083@smail.iitm.ac.in>
2026-04-09 08:50:05 +0530

    comments added

--- lectures/09_effect_handlers/code/basic/test6.ml
+++ lectures/09_effect_handlers/code/basic/test6.ml
@@ -4,6 +4,13 @@ open Effect.Deep
 type _ Effect.t += E : unit t
                  | F : unit t
 
+(*
+exception Unhandled: 'a t -> exn
+(** Raised by [perform] when no handler is found for 
+      the performed effect. *)
+
+--> Exception: Stdlib.Effect.Unhandled(E)
+*)
 let f () =
   try perform E with
   | Unhandled E -> Printf.printf "Caught Unhandled E\n"
